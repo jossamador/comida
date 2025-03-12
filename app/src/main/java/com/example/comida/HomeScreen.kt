6ae985179fc63c9ae6262993e021e0c0da4e658f
jossamador <jos-amador@hotmail.com>
2025-03-12 11:26:21 -0600

    Descripción cambios

--- app/src/main/java/com/example/comida/HomeScreen.kt
+++ app/src/main/java/com/example/comida/HomeScreen.kt
@@ -177,11 +177,11 @@ fun FoodItem(food: Food) {
 }
 
 fun getCategories() = listOf(
-    Category("Fast Food", "https://images.unsplash.com/photo-1655195672440-c8c62902a05c?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-    Category("Chinese", "https://images.unsplash.com/photo-1563245372-f21724e3856d?q=80&w=2762&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-    Category("Italian", "https://images.unsplash.com/photo-1609582848349-215e8bf397d3?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-    Category("Mexican", "https://images.unsplash.com/photo-1628394029816-1dc524670f60?q=80&w=2391&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-    Category("Japanese", "https://images.unsplash.com/photo-1602273660127-a0000560a4c1?q=80&w=2385&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+    Category("Comida Rapida", "https://images.unsplash.com/photo-1655195672440-c8c62902a05c?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    Category("Comida China", "https://images.unsplash.com/photo-1563245372-f21724e3856d?q=80&w=2762&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    Category("Comida Italiana", "https://images.unsplash.com/photo-1609582848349-215e8bf397d3?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    Category("Comida Mexicana", "https://images.unsplash.com/photo-1628394029816-1dc524670f60?q=80&w=2391&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    Category("Comida Japanesa", "https://images.unsplash.com/photo-1602273660127-a0000560a4c1?q=80&w=2385&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
 )
 
 fun getRestaurants() = listOf(
@@ -193,9 +193,9 @@ fun getRestaurants() = listOf(
 )
 
 fun getRecommendedFoods() = listOf(
-    Food("Hamburger", "https://images.unsplash.com/photo-1610970878459-a0e464d7592b?q=80&w=2724&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 4.5f, 5.99),
+    Food("Hamburguesa", "https://images.unsplash.com/photo-1610970878459-a0e464d7592b?q=80&w=2724&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 4.5f, 5.99),
     Food("Pizza", "https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?q=80&w=2676&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 4.8f, 8.99),
-    Food("Fried Chicken", "https://plus.unsplash.com/premium_photo-1683139918895-06f8f3b9939e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 4.3f, 6.99),
+    Food("Pollo Frito", "https://plus.unsplash.com/premium_photo-1683139918895-06f8f3b9939e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 4.3f, 6.99),
     Food("Sushi", "https://images.unsplash.com/photo-1579584425555-c3ce17fd4351?q=80&w=2270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pYWdlfHx8fGVufDB8fHx8fA%3D%3D", 4.7f, 12.99),
     Food("Tacos", "https://images.unsplash.com/photo-1599974579688-8dbdd335c77f?q=80&w=2694&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 4.6f, 4.99)
 )
